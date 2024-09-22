8541633931ceaae0d7da12ddee3c3eebd02bb623
stepanmuller <stepanmuller@outlook.com>
2024-09-23 01:24:41 +0200

    Added 345 box system

--- Interaction_manager.h
+++ Interaction_manager.h
@@ -2,16 +2,17 @@
 void update_boxes(int& box_count_x, int& box_count_y, int& box_count)
 {
     //printf("Updating boxes\n");
+    int offset_range = static_cast<int>(ceil(BOX_RANGE) + 0.1);
     float min_x = *(std::min_element(std::begin(x), std::end(x)));
     float min_y = *(std::min_element(std::begin(y), std::end(y)));
     float max_x = *(std::max_element(std::begin(x), std::end(x)));
     float max_y = *(std::max_element(std::begin(y), std::end(y)));
-    box_count_x = static_cast<int>((max_x - min_x) / BOX_SIZE) + 1;
-    box_count_y = static_cast<int>((max_y - min_y) / BOX_SIZE) + 1;
+    box_count_x = static_cast<int>((max_x - min_x) / BOX_SIZE) + 1 + offset_range * 2;
+    box_count_y = static_cast<int>((max_y - min_y) / BOX_SIZE) + 1 + offset_range * 2;
     box_count = box_count_x * box_count_y;
     for (int i = 0; i < PARTICLE_COUNT; i++) {
-        float x_shifted = x[i] - min_x;
-        float y_shifted = y[i] - min_y;
+        float x_shifted = x[i] - min_x + BOX_SIZE * offset_range;
+        float y_shifted = y[i] - min_y + BOX_SIZE * offset_range;
         int box_x = static_cast<int>(x_shifted / BOX_SIZE);
         int box_y = static_cast<int>(y_shifted / BOX_SIZE);
         pbox[i] = box_x + box_y * box_count_x;
@@ -21,7 +22,7 @@ void update_boxes(int& box_count_x, int& box_count_y, int& box_count)
 int get_neighbours(int box_count_x, std::vector<int>& neighbours)
 {
     int neighbour_count = 0;
-    int offset_range = static_cast<int>(ceil(BOX_MULTIPLIER) + 0.1);
+    int offset_range = static_cast<int>(ceil(BOX_RANGE) + 0.1);
     int relative_index = 0;
     for (int offset_y = -offset_range; offset_y <= offset_range; offset_y++) {
         for (int offset_x = -offset_range; offset_x <= offset_range; offset_x++) {
@@ -29,7 +30,7 @@ int get_neighbours(int box_count_x, std::vector<int>& neighbours)
             int j = (abs(offset_y) - 1);
             i = (i < 0) ? 0 : i;
             j = (j < 0) ? 0 : j;
-            if (pow(i, 2) + pow(j, 2) < pow(BOX_MULTIPLIER, 2)) {
+            if (pow(i, 2) + pow(j, 2) < pow((BOX_RANGE - 0.01), 2)) {
                 neighbour_count += 1;
             }
         }
@@ -42,12 +43,13 @@ int get_neighbours(int box_count_x, std::vector<int>& neighbours)
             int j = (abs(offset_y) - 1);
             i = (i < 0) ? 0 : i;
             j = (j < 0) ? 0 : j;
-            if (pow(i, 2) + pow(j, 2) < pow(BOX_MULTIPLIER, 2)) {
+            if (pow(i, 2) + pow(j, 2) < pow((BOX_RANGE - 0.01), 2)) {
                 neighbours[neighbour_count] = offset_x + offset_y * box_count_x;
                 neighbour_count += 1;
             }
         }
     }
+    INTERACTION_AREA_MULTIPLIER = neighbour_count / (pow(BOX_RANGE, 2) * PI);
     return neighbour_count;
 }
 
@@ -82,71 +84,34 @@ int get_I(std::vector<int>& rows, std::vector<int>& cols)
 
     std::vector<int> neighbours;
     int neighbour_count = get_neighbours(box_count_x, neighbours);
-
-    int interaction_count2 = 0;
-    int mybox2 = 0;
-    int friend_box2 = 0;
-    for (int p = 0; p < PARTICLE_COUNT; p++) {
-        mybox2 = pbox[p];
-        for (int i = 0; i < neighbour_count; i++) {
-            friend_box2 = mybox2 + neighbours[i];
-            if (friend_box2 >= 0 && friend_box2 < box_count) {
-                //printf("Adding interaction of mybox %d with friend_box %d resulting from offset_x %d offset_y %d\n", mybox, friend_box, offset_x, offset_y);
-                interaction_count2 = interaction_count2 + box_fill[friend_box2];
-            }
-        }
-    }
-    interaction_count2 = interaction_count2 - PARTICLE_COUNT;
-    printf("interaction_count2 = %d \n", interaction_count2);
-
     int interaction_count = 0;
     int mybox = 0;
-    int mybox_fill = 0;
-    int friend_x = 0;
-    int friend_y = 0;
     int friend_box = 0;
-    for (int i = 0; i < box_count_x; i++) {
-        for (int j = 0; j < box_count_y; j++) {
-            mybox = i + j * box_count_x;
-            mybox_fill = box_fill[mybox];
-            for (int offset_x = -BOX_MULTIPLIER; offset_x <= BOX_MULTIPLIER; offset_x++) {
-                for (int offset_y = -BOX_MULTIPLIER; offset_y <= BOX_MULTIPLIER; offset_y++) {
-                    friend_x = i + offset_x;
-                    friend_y = j + offset_y;
-                    friend_box = friend_x + friend_y * box_count_x;
-                    if (friend_x >= 0 && friend_x < box_count_x && friend_y >= 0 && friend_y < box_count_y) {
-                        //printf("Adding interaction of mybox %d with friend_box %d resulting from offset_x %d offset_y %d\n", mybox, friend_box, offset_x, offset_y);
-                        interaction_count = interaction_count + mybox_fill * box_fill[friend_box];
-                    }
-                }
+    for (int p = 0; p < PARTICLE_COUNT; p++) {
+        mybox = pbox[p];
+        for (int i = 0; i < neighbour_count; i++) {
+            friend_box = mybox + neighbours[i];
+            if (friend_box >= 0 && friend_box < box_count) {
+                //printf("Adding interaction of mybox %d with friend_box %d resulting from offset_x %d offset_y %d\n", mybox, friend_box, offset_x, offset_y);
+                interaction_count = interaction_count + box_fill[friend_box];
             }
-            interaction_count = interaction_count - mybox_fill;
         }
     }
-    printf("interaction_count = %d \n", interaction_count);
+    interaction_count = interaction_count - PARTICLE_COUNT;
     rows.resize(interaction_count);
     cols.resize(interaction_count);
     int counter = 0;
     //printf("Interaction count %d\n", interaction_count);
-    for (int i = 0; i < box_count_x; i++) {
-        for (int j = 0; j < box_count_y; j++) {
-            mybox = i + j * box_count_x;
-            mybox_fill = box_fill[mybox];
-            for (int offset_x = -BOX_MULTIPLIER; offset_x <= BOX_MULTIPLIER; offset_x++) {
-                for (int offset_y = -BOX_MULTIPLIER; offset_y <= BOX_MULTIPLIER; offset_y++) {
-                    friend_x = i + offset_x;
-                    friend_y = j + offset_y;
-                    friend_box = friend_x + friend_y * box_count_x;
-                    if (friend_x >= 0 && friend_x < box_count_x && friend_y >= 0 && friend_y < box_count_y) {
-                        for (int my_particle = 0; my_particle < mybox_fill; my_particle++) {
-                            for (int friend_particle = 0; friend_particle < box_fill[friend_box]; friend_particle++) {
-                                if (box_list[box_cummulative[mybox] + my_particle] != box_list[box_cummulative[friend_box] + friend_particle]) {
-                                    rows[counter] = box_list[box_cummulative[mybox] + my_particle];
-                                    cols[counter] = box_list[box_cummulative[friend_box] + friend_particle];
-                                    counter++;
-                                }
-                            }
-                        }
+    for (int p = 0; p < PARTICLE_COUNT; p++) {
+        mybox = pbox[p];
+        for (int i = 0; i < neighbour_count; i++) {
+            friend_box = mybox + neighbours[i];
+            if (friend_box >= 0 && friend_box < box_count) {
+                for (int friend_particle = 0; friend_particle < box_fill[friend_box]; friend_particle++) {
+                    if (p != box_list[box_cummulative[friend_box] + friend_particle]) {
+                        rows[counter] = p;
+                        cols[counter] = box_list[box_cummulative[friend_box] + friend_particle];
+                        counter++;
                     }
                 }
             }
--- SPH_CPP.cpp
+++ SPH_CPP.cpp
@@ -12,7 +12,6 @@ using namespace std;
 #include "Iterator.h"
 #include "Particle_printer.h"
 
-
 int main()
 {   
     printf("Particle count: %d\n", PARTICLE_COUNT);
@@ -21,13 +20,14 @@ int main()
     int index = generate_fluid_particles(0);
     index = generate_wall_particles(index);
     //print_particles();
-    for (int iteration_count = 0; iteration_count < 1; iteration_count++) {
+    for (int iteration_count = 0; iteration_count < 10001; iteration_count++) {
         run_iteration();
         if (iteration_count % 1000 == 0 && iteration_count > 0) {
             printf("\nFinished iteration %d\n", iteration_count);
             printf("Average calculating time getting I %3.1f ms\n", ((CALCULATING_TIME_BOXES / iteration_count) / 1000));
             printf("Average calculating time interactions %3.1f ms\n",((CALCULATING_TIME_INTERACTIONS / iteration_count) / 1000));
             printf("Average number of interactions %6.1f\n", ((TOTAL_INTERACTIONS/ iteration_count)));
+            printf("Interaction area multiplier %6.3f\n", ((INTERACTION_AREA_MULTIPLIER)));
             //print_particles();
         }
     }
